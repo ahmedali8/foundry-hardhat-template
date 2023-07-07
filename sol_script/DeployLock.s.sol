0692f935c1493cb22e4eca0530ab0934e10f033e
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-07-07 22:20:00 +0500

    refactor: rename deployer to broadcaster feat: use  as broadcaster feat: add FOUNDRY_PROFILE env var

--- sol_script/DeployLock.s.sol
+++ sol_script/DeployLock.s.sol
@@ -1,20 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-import { BaseScript } from "./BaseScript.s.sol";
+import { BaseScript } from "./Base.s.sol";
 import { Lock } from "contracts/Lock.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract DeployLock is BaseScript {
-    Lock internal lock;
-
-    function run() external {
-        // use deployerPrivateKey if private key is used
-        vm.startBroadcast(deployer);
-
+    function run() public broadcast returns (Lock lock) {
         // deploy our contract
         lock = new Lock{ value: 1 }(block.timestamp + 365 days);
-
-        vm.stopBroadcast();
     }
 }
